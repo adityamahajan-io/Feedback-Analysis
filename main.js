33c15661ea9e7c7f31d9c19c6dd319e1f3338b22
adityamahajan-io <adityamahajan.tech@gmail.com>
2020-12-08 18:33:09 +0530

    linux try 1

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { BrowserWindow, app } = require('electron')
+const path =require("path")
 require('./app.js')
 
 let mainWindow = null
-
+iconPath = path.join(__dirname, '/icon/icon.png');
 function main() {
-  mainWindow = new BrowserWindow()
+  mainWindow = new BrowserWindow({icon: iconPath})
   mainWindow.maximize()
   mainWindow.loadURL(`http://localhost:8000/`)
   mainWindow.on('close', event => {
